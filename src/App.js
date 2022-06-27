e01c081f9645ce4c2c6cf10489e5eafa49ebaeac
truongdeveloper <vanminhtruong678@gmail.com>
2022-06-27 16:04:37 +0700

    tests

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>Hello ReactJs</h1>
-        
+        <h2>My name is Trường</h2>
       </header>
     </div>
   );
